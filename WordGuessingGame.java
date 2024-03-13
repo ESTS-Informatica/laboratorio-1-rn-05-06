278b17562685d9eb474ed9be1985aa11c036ea0c
PedroFSerra-Uni <202300247@estudantes.ips.pt>
2024-03-13 09:44:12 +0000

    Nível 2

--- WordGuessingGame.java
+++ WordGuessingGame.java
@@ -10,12 +10,13 @@ public class WordGuessingGame
     private String hiddenWord;
     private String guessedWord;
     private int numberOfTries;
+    private InputReader reader;
     
     public WordGuessingGame(){
         hiddenWord = "abc";
         guessedWord = "___";
         numberOfTries = 0;
-        
+        reader = new InputReader();
     }
     
     public String getHiddenWord(){
@@ -34,5 +35,52 @@ public class WordGuessingGame
         System.out.println(guessedWord);
     }
     
+    public void play(){
+        showWelcome();
+        while (!guessedWord.equals(hiddenWord)){
+            if(guess(reader.getChar("Escreva a letra que pretende adivinhar:"))){
+                System.out.println("Letra correta");
+            }else
+                System.out.println("Letra incorreta");
+            showGuessedWord();
+            numberOfTries++;
+        }
+        showResult();
+    }
     
+    private void showWelcome(){
+        System.out.println("-----------------");
+        System.out.println("Jogo do Enforcado");
+        System.out.println("----Boa Sorte----");
+        System.out.println("-----------------");
+    }
+    
+    private boolean guess(char letra){
+        char[] guessedWordArray = new char[hiddenWord.length()];
+        for(int c = 0; c < guessedWordArray.length; c++){
+            guessedWordArray[c] = guessedWord.charAt(c);
+        }
+        int position = -1;
+        boolean returnValue = false;
+        for(int i = 0; i < hiddenWord.length(); i++){
+            if (hiddenWord.charAt(i) == letra){
+                position = i;
+                for(int b = 0; b < guessedWord.length(); b++){
+                    if(guessedWord.charAt(b) != letra){
+                        guessedWordArray[i] = letra;
+                        returnValue = true;
+                    }
+                }
+            }
+            
+            if(returnValue){
+                guessedWord = new String(guessedWordArray);
+            }
+        }
+        return returnValue;
+    }
+    
+    private void showResult(){
+        System.out.println("A palavra foi adivinhada em " + numberOfTries + ".");
+    }
 }
